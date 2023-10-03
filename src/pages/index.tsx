b0335469a9fa77f3644e54d3e71d27ac7d1b77fa
Ceb <101688741+sebf45@users.noreply.github.com>
2023-10-03 17:07:21 +0700

    init landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,114 +5,153 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+      <main className="bg-slate-900 text-gray-100">
+        <div className="mx-auto max-w-screen-lg px-3 py-6">
+          <div className="flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between"><a href="/">
+            <div
+                className="flex items-center bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-xl font-bold text-transparent">
+              <svg className="mr-1 h-10 w-10 stroke-cyan-600" xmlns="http://www.w3.org/2000/svg"
+                   viewBox="0 0 24 24" fill="none" strokeWidth="1.5" strokeLinecap="round"
+                   strokeLinejoin="round">
+                <path d="M0 0h24v24H0z" stroke="none"></path>
+                <rect x="3" y="12" width="6" height="8" rx="1"></rect>
+                <rect x="9" y="8" width="6" height="12" rx="1"></rect>
+                <rect x="15" y="4" width="6" height="16" rx="1"></rect>
+                <path d="M4 20h14"></path>
+              </svg>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              Cebbb's Playground
+            </div>
           </a>
+            <nav>
+              <ul className="flex gap-x-3 font-medium text-gray-200">
+                <li className="hover:text-white"><a href="/posts/">Blogs</a></li>
+                <li className="hover:text-white"><a href="/">GitHub</a></li>
+                <li className="hover:text-white"><a href="/login">Login</a></li>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="mx-auto max-w-screen-lg px-3 py-6">
+          <div className="flex flex-col items-center md:flex-row md:justify-between md:gap-x-24">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            <div><h1 className="text-3xl font-bold">Xin chào, I'm <span
+                className="bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent">Cebbb</span> 👋
+            </h1><p className="mt-6 text-xl leading-9">A developer from Hanoi, Vietnam.
+              Love <a className="text-cyan-400 hover:underline" href="/">coding</a> and
+              want to make funny webapp and help people for something. This is my <a
+                  className="text-cyan-400 hover:underline" href="/">playground</a>. Really happy if you can
+              find something funny here. I hope giving you smile and a little positive energy for your day.
+              <br/> Luv ya !!!</p>
+              <div className="mt-3 flex gap-1"><a href="/"><img className="h-12 w-12 hover:translate-y-1"
+                                                                src="/assets/twitter-icon.png"
+                                                                alt="Twitter icon" loading="lazy"></img></a><a
+                  href="/"><Image width={100} height={100} className="h-12 w-12 hover:translate-y-1"
+                                  src="/assets/facebook-icon.png" alt="Facebook icon" loading="lazy"></Image></a><a
+                  href="/"><Image width={100} height={100} className="h-12 w-12 hover:translate-y-1"
+                                  src="/assets/linkedin-icon.png" alt="Linkedin icon" loading="lazy"></Image></a><a
+                  href="/"><Image width={100} height={100} className="h-12 w-12 hover:translate-y-1"
+                                  src="/assets/youtube-icon.png" alt="Youtube icon"
+                                  loading="lazy"></Image></a>
+              </div>
+            </div>
+            <div className="shrink-0"><Image width={100} height={100} className="h-80 w-64" src="/assets/avatar.svg"
+                                             alt="Avatar image" loading="lazy"></Image></div>
+          </div>
+        </div>
+        <div className="mx-auto max-w-screen-lg px-3 py-6">
+          <div className="mb-6 text-2xl font-bold">Recent <span
+              className="bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent">Projects</span>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
+              <div className="shrink-0"><a href="/"><Image width={100} height={100} className="h-36 w-36 hover:translate-y-1"
+                                                           src="/assets/project-web-design.png"
+                                                           alt="Project Web Design"
+                                                           loading="lazy"></Image></a></div>
+              <div>
+                <div className="flex flex-col items-center gap-y-2 md:flex-row"><a
+                    className="hover:text-cyan-400" href="/">
+                  <div className="text-xl font-semibold">Project 1</div>
+                </a>
+                  <div className="ml-3 flex gap-2">
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">Astro.js
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-lime-400 text-lime-900">Web
+                      design
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-sky-400 text-sky-900">Tailwind.css
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">TypeScript
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-3 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p></div>
+            </div>
+            <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
+              <div className="shrink-0"><a href="/"><Image width={100} height={100} className="h-36 w-36 hover:translate-y-1"
+                                                           src="/assets/project-fire.png"
+                                                           alt="Project Fire" loading="lazy"></Image></a>
+              </div>
+              <div>
+                <div className="flex flex-col items-center gap-y-2 md:flex-row"><a
+                    className="hover:text-cyan-400" href="/">
+                  <div className="text-xl font-semibold">Project 2</div>
+                </a>
+                  <div className="ml-3 flex gap-2">
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-violet-400 text-violet-900">Next.js
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-emerald-400 text-emerald-900">Blog
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-yellow-400 text-yellow-900">JavaScript
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-3 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p></div>
+            </div>
+            <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
+              <div className="shrink-0"><a href="/"><Image width={100} height={100}
+                                                           className="h-36 w-36 hover:translate-y-1"
+                                                           src="/assets/project-maps.png"
+                                                           alt="Project Maps" loading="lazy"></Image></a>
+              </div>
+              <div>
+                <div className="flex flex-col items-center gap-y-2 md:flex-row"><a
+                    className="hover:text-cyan-400" href="/">
+                  <div className="text-xl font-semibold">Project 3</div>
+                </a>
+                  <div className="ml-3 flex gap-2">
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">Astro.js
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-indigo-400 text-indigo-900">Bootstrap
+                    </div>
+                    <div
+                        className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">TypeScript
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-3 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse
+                  bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+                  hendrerit dui odio id enim.</p></div>
+            </div>
+          </div>
+        </div>
+      </main>
   )
 }
